7cafeb9af33f472d91fa05e5c39f30a46a9164bb
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-11-11 21:48:17 +0300

    Полу добавлены методы

--- src/Model/GenderInterface.php
+++ src/Model/GenderInterface.php
@@ -7,5 +7,28 @@ namespace Maris\interfaces\Person\Model;
  */
 interface GenderInterface
 {
+    /**
+     * Указывает что пол мужской.
+     * @return bool
+     */
+    public function isMan():bool;
 
+    /**
+     * Указывает что пол женский.
+     * @return bool
+     */
+    public function isGirl():bool;
+
+    /**
+     * Указывает что пол не определен.
+     * @return bool
+     */
+    public function isUnknown():bool;
+
+    /**
+     * Указывает что пол определен.
+     * Полная противоположность static::isUnknown()
+     * @return bool
+     */
+    public function isSuccess():bool;
 }
